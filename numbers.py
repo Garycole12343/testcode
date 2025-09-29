867c33cfed327511016d8518fb5580cfb3b8e788
aws.admin <2512955@leedstrinity.ac.uk>
2025-09-29 14:02:53 +0100

    new

---
+++ numbers.py
@@ -0,0 +1,11 @@
+user_height_cm = 140
+x=str(140)
+print(x)
+
+neighbour1_heigh_cm = 160
+neighbour2_height_cm = 120
+z=str(160)
+y=str(120)
+print(x,y,z)
+mean=str(160+140+120/3)
+print(mean)
---
+++ strings.py
@@ -0,0 +1,17 @@
+print ('Hello World')
+
+
+a= "Hello World"
+print(len(a))
+
+txt = "Hello World"
+
+x=txt.lower()
+
+print (x)
+
+txt = "Hello World"
+
+x=txt.upper()
+
+print (x)
