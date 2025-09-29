2fc90dae3dd8cd9b2609b732b7c8457557d1bb53
aws.admin <2512955@leedstrinity.ac.uk>
2025-09-29 14:20:27 +0100

    new

--- numbers.py
+++ numbers.py
@@ -9,3 +9,17 @@ y=str(120)
 print(x,y,z)
 mean=str(160+140+120/3)
 print(mean)
+
+centi = float("340")
+inch = centi / 2.54
+print (centi,"cm in inches is",inch)
+rounded_number = round(133.85826771653544, 2)
+print(rounded_number)
+
+inch = float("133.86")
+feet = inch / 12
+print (inch, "inch in feet is", feet)
+rounded_number = round(11.155000000000001)
+print(rounded_number)
+remaining_inches=inch/ 12
+print(remaining_inches - 11)
